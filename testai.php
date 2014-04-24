209384b07538838b01e94d9e125c19e8a0e800eb
amontano3 <relevantparadox@gmail.com>
2014-04-24 12:05:28 -0400

    Updated Big Money/Smithy
    An error still exists when trying to play a smithy. Currently trying to
    debug.

--- testai.php
+++ testai.php
@@ -3,6 +3,62 @@
 chdir('ai');
 include 'lib/common.php';
 
+// Big Money/ Big Smithy
+$hasSmithy = false;
+$numSmithys = -1;
+$stacks=$GameState['actionStacks'];
+foreach ($stacks as $cardName)
+{
+	if ($cardName['cardName'] == 'smithy')
+	{
+		$hasSmithy = true;
+		$numSmithys = numOfCardsOwned('smithy');
+	}
+}
+
+$actions = 1;
+$hand = $GameState['players'][$GameState['currentPlayer']]['hand'];
+while($actions != 0)
+{
+	if($hasSmithy == true)
+	{
+		foreach ($hand as $cardName)
+		{
+			if ($cardName == 'smithy')
+			{
+				smithy();
+				$actions--;
+			}
+		}
+	}
+	$actions--;
+}
+$coins = count_money();
+play_money();
+$numSmithys = numOfCardsOwned('smithy');
+if($coins >= 4 and $hasSmithy == true and $numSmithys == 0)
+{
+	buy_card('smithy');
+}
+else if($coins >= 8)
+{
+	buy_card('province');
+}
+else if($coins == 6 or $coins == 7)
+{
+	buy_card('gold');
+}
+else if($coins == 5 or $coins == 4 or $coins == 3)
+{
+	buy_card('silver');
+}
+/*
+else if($coins == 5 or $coins == 4 or $coins == 3)
+{
+	buy_card('silver');
+}
+*/
+
 echo json_encode($playerResponse); // playerResponse is an instance of PlayerResponse
 
 ?>
